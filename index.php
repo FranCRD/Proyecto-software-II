e5450a6ffe1b8bbcdf5a7ee48e2591b6bc8d0bee
FranCRD <francocordoba07@gmail.com>
2025-11-04 17:59:59 -0600

    Add files via upload
    Index php del proyecto final

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="Index.css"></link>
+</head>
+<body>
+    <header>
+    <header class="header">
+    <div class="logo">TSUR</div>
+    <nav class="nav">
+      <a href="index.php">Inicio</a>
+      <a href="#">Sobre nosotros</a>
+      <a href="citas.php">Citas</a>
+      <a href="login.php">Iniciar sesión</a>
+    </nav>
+  </header>
+
+    <!-- ===== SECCIÓN PRINCIPAL ===== -->
+    <main class="hero">
+        <div class="hero-content">
+            <h1>Construimos confianza, no solo proyectos</h1>
+            <p>En TSUR ofrecemos asesoría técnica y consultoría en construcción, garantizando calidad, seguridad y eficiencia en cada obra.</p><br>
+            <button>Solicitar asesoría</button> <!-- agregar enlace para que funcione el boton -->
+        </div>
+    </main>
+
+<footer class="footer"> 
+    <div class="footer-content">
+    <p>© 2024 TSUR. Todos los derechos reservados.</p>
+    </div>
+</footer>
+</body>
+</html>
